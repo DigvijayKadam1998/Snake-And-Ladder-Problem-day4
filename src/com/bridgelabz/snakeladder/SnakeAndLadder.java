e8dac9b802bdcc709ce362d5e873fdcf38f0ae7f
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 23:52:07 +0530

    Added: Player Reaches the Winning Position

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -4,28 +4,33 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
     	int position = 0;
-	System.out.println("Player at Starting Position: " + position);
+    	final int WINNING_POSITION = 100;
+    	System.out.println("Player at Starting Position: " + position);
 		
-	// taking random input for Generating number for disc between 1 to 6
-	Random rd = new Random(); 
-	int diceValue = rd.nextInt(6)+1;
-	System.out.println("Getting number when you roll a die: " + diceValue);
+    	while(position <= WINNING_POSITION ) {
+			
+    	    Random rd = new Random();
+    	    int diceValue = rd.nextInt(6)+1;
+    	    System.out.println("Getting number when you roll Dice: " + diceValue);
 		
-	// by using if else condition check for option no play, ladder, snake
-	int checkOption = rd.nextInt(3);
-		
-	if(checkOption == 0) {
-	    System.out.println("No Play");
-	}
-	else if(checkOption == 1) {
-	    position += diceValue;		//position = position + diceValue;
-	    System.out.println("Ladder");
-	    System.out.println("Your Current Position is: " + position);
-	}
-	else {
-	    position -= diceValue;
-	    System.out.println("Snake");
-	    System.out.println("Your Current Position is: " + position);
-	}
+    	    int checkOption = rd.nextInt(3);
+    	    if(checkOption == 0) {
+    		System.out.println("No Play");
+    	    }
+    	    else if(checkOption == 1) {
+    		position += diceValue;
+    		System.out.println("Ladder");
+    		System.out.println("Your Current Position is: " + position);
+    	    }
+    	    else if (checkOption == 2){
+    		position -= diceValue;
+    		System.out.println("Snake");
+    		System.out.println("Your Current Position is: " + position);
+    	    }
+    	    if (position < 0) {
+    		position = 0;
+    		System.out.println("Current Position is ==>" + position);
+    	    }
+        }
     }
 }
