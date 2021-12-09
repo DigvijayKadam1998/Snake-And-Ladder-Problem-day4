cc197d8b700b9d92053b458c84a344dea3c39afb
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-10 00:07:21 +0530

    Added: Player get to Exact Winning Position

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -4,33 +4,49 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
     	int position = 0;
-    	final int WINNING_POSITION = 100;
-    	System.out.println("Player at Starting Position: " + position);
+	final int WINNING_POSITION = 100;
 		
-    	while(position <= WINNING_POSITION ) {
+	//using while loop
+	while(position <= WINNING_POSITION) { 
 			
-    	    Random rd = new Random();
-    	    int diceValue = rd.nextInt(6)+1;
-    	    System.out.println("Getting number when you roll Dice: " + diceValue);
+	    Random rand = new Random();
+	    int diceValue = rand.nextInt(6)+1;
+	    System.out.println("Dice Value is ==> " +diceValue);
+	    int option = rand.nextInt(3);
 		
-    	    int checkOption = rd.nextInt(3);
-    	    if(checkOption == 0) {
-    		System.out.println("No Play");
-    	    }
-    	    else if(checkOption == 1) {
-    		position += diceValue;
-    		System.out.println("Ladder");
-    		System.out.println("Your Current Position is: " + position);
-    	    }
-    	    else if (checkOption == 2){
-    		position -= diceValue;
-    		System.out.println("Snake");
-    		System.out.println("Your Current Position is: " + position);
-    	    }
-    	    if (position < 0) {
-    		position = 0;
-    		System.out.println("Current Position is ==>" + position);
-    	    }
-        }
+	    if(position == WINNING_POSITION) {
+	        System.out.println("<====You Won The Game====>");
+	        break;
+	    }
+			
+	    //check condition in switch case 
+	    switch(option) {
+	    case 0:
+		System.out.println("No Play");
+		break;
+	    case 1:
+		System.out.println("Ladder");
+		position += diceValue;
+		System.out.println("Current Position is ==>" + position);
+
+		if(position > 100) {
+		    position = position - diceValue;
+		    System.out.println("Current Position is ==>" + position);
+		}
+		break;
+	    case 2:
+		System.out.println("Snake");
+		position -= diceValue;
+		System.out.println("Current Position is ==>" + position);
+				
+		if(position < 0) {
+		    position = 0;
+		    System.out.println("Current Position is ==>" + position);
+		}
+	        break;
+	    default:
+	        System.out.println("Not Correct");
+	    }
+	}
     }
 }
