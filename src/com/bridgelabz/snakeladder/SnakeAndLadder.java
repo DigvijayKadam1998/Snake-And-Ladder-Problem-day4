ec3be5b0f4b5bd65b56e0dce02c9db97230e1d44
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 23:40:36 +0530

    Added: Checking for Option

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -6,9 +6,26 @@ public class SnakeAndLadder {
     	int position = 0;
 	System.out.println("Player at Starting Position: " + position);
 		
-	// taking random input for Generating number between 1 to 6
-	Random rd = new Random();
-	int diceValue = rd.nextInt(6) + 1;
+	// taking random input for Generating number for disc between 1 to 6
+	Random rd = new Random(); 
+	int diceValue = rd.nextInt(6)+1;
 	System.out.println("Getting number when you roll a die: " + diceValue);
+		
+	// by using if else condition check for option no play, ladder, snake
+	int checkOption = rd.nextInt(3);
+		
+	if(checkOption == 0) {
+	    System.out.println("No Play");
+	}
+	else if(checkOption == 1) {
+	    position += diceValue;		//position = position + diceValue;
+	    System.out.println("Ladder");
+	    System.out.println("Your Current Position is: " + position);
+	}
+	else {
+	    position -= diceValue;
+	    System.out.println("Snake");
+	    System.out.println("Your Current Position is: " + position);
+	}
     }
 }
