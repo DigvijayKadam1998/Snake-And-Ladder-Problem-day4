e105d0e4c3b53bfec4a89be7a78b734a6040750e
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-09 23:33:53 +0530

    Added: Player Rolled Dice

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.snakeladder;
 
+import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
     	int position = 0;
-	System.out.println("Welcome to Snake and Ladder Program");
-	System.out.println("Player at Starting Position: " +position);
+	System.out.println("Player at Starting Position: " + position);
+		
+	// taking random input for Generating number between 1 to 6
+	Random rd = new Random();
+	int diceValue = rd.nextInt(6) + 1;
+	System.out.println("Getting number when you roll a die: " + diceValue);
     }
 }
